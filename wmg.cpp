dedb5943b28d70af4325b1c1e4747b0cf84a9e2b
U-BO2YJFY3ACDX59I\Administrator <wmgang@163.com>
2017-07-18 11:19:55 +0800

    test merge

--- wmg.cpp
+++ wmg.cpp
@@ -5,6 +5,58 @@ void changeToNetData(unsigned short *data, int num)
     for ( i = 0; i < num; i++)
     {
         data[i] = htons(data[i]);
+
+int merge brach;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	int wmg = 0;
     }
 }
 
